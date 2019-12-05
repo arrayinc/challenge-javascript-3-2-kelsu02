5bcab02d75ac70aec2439f862a87563559851862
kredmond <kelsu02@gmail.com>
2019-12-05 12:32:21 -0700

    Finished Rhyme.

--- answer.js
+++ answer.js
@@ -1,1 +1,14 @@
 // *** YOUR ANSWER BELOW ***
+let items = ["thumb", "shoe", "knee", "door", "hive", "stick", "heaven", "pate", "spine", "shin"];
+
+function oldMan(number) {
+    let rhyme = 0;
+    for (let i = 1; i <= number; i++) {
+        console.log(`This old man, he played ${i}.`);
+        console.log(`He played knick-knack on my ${items[rhyme]}. With a knick-knack, paddy-whack, Give your dog a bone.`);
+        console.log(`This old man came rolling home.`);
+        rhyme += 1;
+    }
+}
+
+oldMan(10, items);
